311612cbdb1fd5ec522d2e48ed98916accdb46c1
nal10 <nalusk225@gmail.com>
2023-07-31 16:00:35 -0700

    Update classical_filter.py

--- src/cellfinder_core/detect/filters/plane/classical_filter.py
+++ src/cellfinder_core/detect/filters/plane/classical_filter.py
@@ -11,8 +11,10 @@ def enhance_peaks(img, clipping_value, gaussian_sigma=2.5):
     filtered_img *= -1
     
     filtered_img -= filtered_img.min()
-    #filtered_img = np.nan_to_num(filtered_img)    
-    filtered_img /= filtered_img.max()
+    filtered_img = np.nan_to_num(filtered_img)
+    
+    if filtered_img.max() != 0:
+        filtered_img /= filtered_img.max()
 
     # To leave room to label in the 3d detection.
     filtered_img *= clipping_value
